780fcb0cd545c5bc53e36d95a217d51746d64684
Dmytro Hubatenko <gubatenkod@gmail.com>
2021-07-31 19:53:27 +0300

    Added FeatureType enum

---
+++ inc/types/featuretype.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+namespace OC {
+
+    struct FeatureType
+    {
+        static const std::string VERSIONS;
+        static const std::string PROFILES;
+        static const std::string PAIRS;
+        static const std::string RATE_LIMIT;
+    };
+
+}
---
+++ src/types/featuretype.cpp
@@ -0,0 +1,8 @@
+#include "featuretype.h"
+
+using namespace OC;
+
+const std::string FeatureType::VERSIONS = "versions";
+const std::string FeatureType::PROFILES = "profiles";
+const std::string FeatureType::PAIRS = "pairs";
+const std::string FeatureType::RATE_LIMIT = "rate_limit";
