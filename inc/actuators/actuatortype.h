003977a15ae553fbaf06573a5f0e85f641526e57
Dmytro Hubatenko <gubatenkod@gmail.com>
2021-07-31 14:12:46 +0300

    Updated ActuatorType
    Finished translating ActuatorType

--- inc/actuators/actuatortype.h
+++ inc/actuators/actuatortype.h
@@ -4,9 +4,31 @@
 
 namespace OC {
 
+    /**
+     * Definition of all the allowed actuator types as of the 03/27/2017 Openc2 spec
+     */
     struct ActuatorType
     {
         static const std::string SLPF;
+        //static const std::string ENDPOINT;,
+        //static const std::string ENDPOINT_WORKSTATION;
+        //static const std::string ENDPOINT_SERVER;
+        //static const std::string NETWORK;
+        //static const std::string NETWORK_FIREWALL;
+        //static const std::string NETWORK_ROUTER;
+        //static const std::string NETWORK_PROXY;
+        //static const std::string NETWORK_SENSOR;
+        //static const std::string NETWORK_HIPS;
+        //static const std::string NETWORK_SENSE_MAKING;
+        //static const std::string PROCESS;
+        //static const std::string PROCESS_ANTI_VIRUS_SCANNER;
+        //static const std::string PROCESS_AAA_SERVICE;
+        //static const std::string PROCESS_VIRTUALIZATION_SERVICE;
+        //static const std::string PROCESS_SANDBOX;
+        //static const std::string PROCESS_EMAIL_SERVICE;
+        //static const std::string PROCESS_DIRECTORY_SERVICE;
+        //static const std::string PROCESS_REMEDIATION_SERVICE;
+        //static const std::string PROCESS_LOCATION_SERVICE;
     };
 
 }
--- src/actuators/actuatortype.cpp
+++ src/actuators/actuatortype.cpp
@@ -3,3 +3,23 @@
 using namespace OC;
 
 const std::string ActuatorType::SLPF = "slpf";
+
+//const std::string ActuatorType::ENDPOINT = "endpoint";
+//const std::string ActuatorType::ENDPOINT_WORKSTATION = "endpoint_workstation";
+//const std::string ActuatorType::ENDPOINT_SERVER = "endpoint_server";
+//const std::string ActuatorType::NETWORK = "network";
+//const std::string ActuatorType::NETWORK_FIREWALL = "network_firewall";
+//const std::string ActuatorType::NETWORK_ROUTER = "network_router";
+//const std::string ActuatorType::NETWORK_PROXY = "network_proxy";
+//const std::string ActuatorType::NETWORK_SENSOR = "network_sensor";
+//const std::string ActuatorType::NETWORK_HIPS = "network_hips";
+//const std::string ActuatorType::NETWORK_SENSE_MAKING = "network_sense_making";
+//const std::string ActuatorType::PROCESS = "process";
+//const std::string ActuatorType::PROCESS_ANTI_VIRUS_SCANNER = "process_anti_virus_scanner";
+//const std::string ActuatorType::PROCESS_AAA_SERVICE = "process_aaa_service";
+//const std::string ActuatorType::PROCESS_VIRTUALIZATION_SERVICE = "process_virtualization_service";
+//const std::string ActuatorType::PROCESS_SANDBOX = "process_sandbox";
+//const std::string ActuatorType::PROCESS_EMAIL_SERVICE = "process_email_service";
+//const std::string ActuatorType::PROCESS_DIRECTORY_SERVICE = "process_directory_service";
+//const std::string ActuatorType::PROCESS_REMEDIATION_SERVICE = "process_remediation_service";
+//const std::string ActuatorType::PROCESS_LOCATION_SERVICE = "process_location_service";
