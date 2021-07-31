664bda1be09877d17f11a1b13e6dfae591a9d638
Dmytro Hubatenko <gubatenkod@gmail.com>
2021-07-31 19:59:02 +0300

    Added HashType enum

---
+++ inc/types/hashtype.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace OC {
+
+    struct HashType
+    {
+        static const std::string MD5;
+        static const std::string SHA1;
+        static const std::string SHA256;
+    };
+
+}
---
+++ src/types/hashtype.cpp
@@ -0,0 +1,7 @@
+#include "hashtype.h"
+
+using namespace OC;
+
+const std::string HashType::MD5 = "md5";
+const std::string HashType::SHA1 = "sha1";
+const std::string HashType::SHA256 = "sha256";
