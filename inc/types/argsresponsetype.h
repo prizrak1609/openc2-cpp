707e49f4428105b84cdd73156655c0acdfc79fa5
Dmytro Hubatenko <gubatenkod@gmail.com>
2021-07-31 19:50:20 +0300

    Added ArgsResponseType enum

---
+++ inc/types/argsresponsetype.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+namespace OC {
+
+    struct ArgsResponseType
+    {
+        static const std::string NONE;
+        static const std::string ACK;
+        static const std::string STATUS;
+        static const std::string COMPLETE;
+    };
+
+}
---
+++ src/types/argsresponsetype.cpp
@@ -0,0 +1,8 @@
+#include "argsresponsetype.h"
+
+using namespace OC;
+
+const std::string ArgsResponseType::NONE = "none";
+const std::string ArgsResponseType::ACK = "ack";
+const std::string ArgsResponseType::STATUS = "status";
+const std::string ArgsResponseType::COMPLETE = "complete";
